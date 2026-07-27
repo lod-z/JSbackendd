4df65fa50e9a9f3fc628785c10825ec67481c2dd
lod-z <nicolassalmeida10@gmail.com>
2026-07-27 12:02:26 -0300

    Add files via upload

---
+++ jsbackend/script.js
@@ -0,0 +1,39 @@
+//1. Usando var (forma mais antiga)
+var idade = 25;
+var nome = "Maria";
+var estudante = true;
+
+//2; Usando let (introduzido no ES6)
+let idade = 25;
+let nome = "Maria"
+let estudante = true;
+
+//3. Usando const (para valores constantes)
+const PI = 3.14159;
+const NOME_EMPRESA = "TechSolutions";
+
+
+//Tipos de dados em Java Script
+//1. Tipos Primitivos
+Number (Número)
+//representa tato números inteiros quanto decimais.
+let idade = 25; //Número inteiro
+let altura = 1.75; //Número decimal
+let temperatura = -5; //Número negativo
+
+String (texto)
+//representa sequência de caracteres (textos)
+let nome = "joão";
+let sobrenome = "silva";
+let endereco = "Rua das Flores, 123"; // Template string (ES6)
+
+Boolean (Booleano)
+//Representa valores lógicos: verdadeiro ( true ) ou falso ( false )
+let estudante = true;
+let aprovado = false;
+undefined
+//Representa uma variável que foi declarada, mas não recebeu um valor.
+let cidade; //valor é undefined
+null
+//Representa a ausência intencional de valor.
+let telefone = null; //Explicitamente sem valor    
