b356a12402bc5d599e0bc9934acce07abb01ae4e
lod-z <nicolassalmeida10@gmail.com>
2026-08-10 11:07:59 -0300

    Add files via upload

---
+++ vscode/hello/hello.js
@@ -0,0 +1,4 @@
+console.log("Olá, mundo! Este é o meu primeiro script Node.js!");
+console.log("Parabéns! Você está programando!");
+console.log("Node.js versão: ", process.version);
+console.log("Sistema operacional: ", process.plataform);
